667cd399df4ca4b0e1f22e0007414edcb1790e9a
erelsgl <erelsgl@gmail.com>
2016-03-17 21:30:19 +0200

    update js files

--- home/main.js
+++ home/main.js
@@ -1,5 +1,7 @@
 /*
  * To bundle this file, use: 
       browserify main.js -o main.bundle.js
+ *     or: 
+      browserify main.js | uglifyjs - -m -c -o main.bundle.js
  */
 window.CompensationProcedure = require("./compensation");
